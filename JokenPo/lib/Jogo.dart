8e5a25f53c5d72b694783e15aca34b23810ab434
ThaysMrtns <thays.martins@nave.org.br>
2021-01-18 13:22:38 -0300

    Importaçao de imagens

--- JokenPo/lib/Jogo.dart
+++ JokenPo/lib/Jogo.dart
@@ -8,6 +8,23 @@ class Jogo extends StatefulWidget {
 class _State extends State<Jogo> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JokenPo"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "Escolha do App:",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              )),
+          Image.asset("")
+        ],
+      ),
+    );
   }
 }
