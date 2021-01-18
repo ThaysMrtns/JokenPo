a83cab6e91f7a46461e0bd33c6df56664b60563f
ThaysMrtns <thays.martins@nave.org.br>
2021-01-18 12:53:12 -0300

    Importando widget no arquivo main

---
+++ JokenPo/android/app/src/main/kotlin/com/example/JokenPo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.JokenPo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ JokenPo/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:JokenPo/Jogo.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Jogo(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
